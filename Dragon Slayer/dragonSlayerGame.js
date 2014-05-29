519e7a1f6068d80ab99a69c7ff2c8dc3681e96f1
Michael Swisher <Michael.V.Swisher@colorado.edu>
2014-05-28 23:19:49 -0600

    Uploading Dragon Slayer Game

---
+++ Dragon Slayer/dragonSlayerGame.js
@@ -0,0 +1,16 @@
+var slaying = true;
+var youHit = Math.floor(Math.random() * 2);
+var damageThisRound = Math.floor(Math.random() * 5 + 1);
+var totalDamage = 0;
+while (slaying) {
+    if (youHit) {
+        console.log("You hit the dragon!");
+        totalDamage += damageThisRound;
+        if (totalDamage >= 4)
+            slaying = false;
+        console.log("Congratulations, you defeated the dragon!");
+    }
+    else
+        console.log("The dragon defeated you");
+    slaying = false;
+}
