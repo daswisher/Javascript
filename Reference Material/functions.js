0d46ea6274f1ffd538e47f6d1c605f77599e1d92
Michael Swisher <Michael.V.Swisher@colorado.edu>
2014-07-27 20:23:11 -0600

    Reorganizing directories and setting up the jQuery subdirectory to follow the Projects/Reference Material organizational structure

--- Reference Material/functions.js
+++ Reference Material/functions.js
@@ -1,3 +1,6 @@
+//Functions are created the same way that variables are within JavaScript
+//The function can take in parameters even though it doesn't necessarily need to
+
 var sleepCheck = function(numHours){
 	if(numHours>=8){
 		return "That's plent of sleep!";
